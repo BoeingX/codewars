9d47590dd8c797d8008b02649fa79488e683f291
BoeingX <12154983+BoeingX@users.noreply.github.com>
2019-03-12 21:34:28 +0800

    Use Data.Graph for topological sort

--- src/RecoverSecretFromTriplets.hs
+++ src/RecoverSecretFromTriplets.hs
@@ -1,29 +1,12 @@
 module RecoverSecretFromTriplets where
 
-import Data.List
-
-type Node = Char
-type Edge = (Char, Char)
-type Graph = ([Node], [Edge])
-
-mkGraph :: [String] -> Graph
-mkGraph xs = (ns, es)
-    where es = concatMap (\[x,y,z] -> [(x,y), (y,z), (x,z)]) xs
-          ns = nub $ concat xs
-
-inDegree :: Graph -> Node -> Int
-inDegree (_, es) n = length $ filter (\(f, t) -> t == n) es
-
-removeNode :: Graph -> Node -> Graph
-removeNode (ns, es) n = (ns', es')
-    where ns' = delete n ns
-          es' = filter (\(f, t) -> f /= n && t /= n) es
-
-tsort :: Graph -> [Node]
-tsort ([], []) = []
-tsort g@(ns, _) = n : tsort g'
-    where n = fst $ head $ filter (\(node, deg) -> deg == 0) $ map (\n -> (n, inDegree g n)) ns
-          g' = removeNode g n
+import Data.List (nub)
+import Data.Graph
+import Data.Map (fromList, (!))
 
 recoverSecret :: [String] -> String
-recoverSecret = tsort . mkGraph
+recoverSecret xs = map (i2v !) $ topSort $ buildG (1, length vs) es
+    where vs = nub $ concat xs
+          v2i = fromList $ zip vs [1..]
+          i2v = fromList $ zip [1..] vs
+          es = concatMap (\[x,y,z] -> [(x, y), (y, z), (x, z)]) $ map (map (v2i !)) xs
