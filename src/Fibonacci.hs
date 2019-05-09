d2f7647dd484875896d709980c5c9644845a2e24
BoeingX <12154983+BoeingX@users.noreply.github.com>
2019-05-09 19:20:36 +0200

    Add solution to the fibonacci kata

--- src/Fibonacci.hs
+++ src/Fibonacci.hs
@@ -1,14 +1,24 @@
+-- See https://www.codewars.com/kata/the-millionth-fibonacci-kata/haskell
 module Fibonacci where
 
-import Data.List
+newtype Matrix a = Matrix (a, a, a, a)
 
--- For fib n, n >= 0
--- fibs = [0, 1, 1, 2, 3, 5, 8, ...]
-fibs :: [Integer]
-fibs = scanl' (+) 0 (1:fibs)
+-- We only implement the multiplication
+instance (Num a) => Num (Matrix a) where
+    Matrix (a11, a12, a21, a22) * Matrix (a11', a12', a21', a22') = Matrix (a11'', a12'', a21'', a22'')
+        where a11'' = a11 * a11' + a12 * a21'
+              a12'' = a11 * a12' + a12 * a22'
+              a21'' = a21 * a11' + a22 * a21'
+              a22'' = a21 * a12' + a22 * a22'
+
+toFib :: Matrix Integer -> Integer
+toFib (Matrix (a, _, _, _)) = a
+
+e :: Matrix Integer
+e = Matrix (0, 1, 1, 1)
 
 fib :: Integer -> Integer
 fib n
-    | n >= 0    = fibs !! fromInteger n
-    | otherwise = (fibs !! fromInteger (-n)) * k
+    | n >= 0     = toFib $ e ^ (n + 1)
+    | otherwise = fib (-n) * k
     where k = if n `mod` 2 == 0 then -1 else 1
